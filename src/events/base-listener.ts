fddce7a92957b481fc8d78b73381a1166390ce89
roman.strazanec <strazanec.roman@gmail.com>
2021-06-06 22:59:00 +0200

    Updates

--- src/events/base-listener.ts
+++ src/events/base-listener.ts
@@ -10,7 +10,7 @@ export abstract class Listener<T extends Event> {
   readonly abstract subject: T['subject'];
   readonly abstract queueGroupName: string;
   abstract onMessage(data: T['data'], msg: Message): void;
-  private client: Stan;
+  protected client: Stan;
   protected ackWait = 5 * 1000;
 
   constructor(client: Stan) {
